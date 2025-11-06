57c8d58d2a30dcd0871f2f622e8422c75d894784
Kyraminol Endyeran <kyraminari@gmail.com>
2025-11-06 09:48:51 +0100

    [nvim] update leap config

--- dot_config/nvim/lua/plugins/leap.lua
+++ dot_config/nvim/lua/plugins/leap.lua
@@ -1,6 +1,20 @@
-return { {
-	"ggandor/leap.nvim",
-	config = function()
-		require("leap").set_default_mappings()
-	end,
-} }
+return {
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			local leap = require("leap")
+			vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+			vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
+			leap.opts.equivalence_classes = {
+				" \t\r\n",
+				"([{",
+				")]}",
+				"'\"`",
+			}
+			leap.opts.preview = function(ch0, ch1, ch2)
+				return not (ch1:match("%s") or (ch0:match("%a") and ch1:match("%a") and ch2:match("%a")))
+			end
+			require("leap.user").set_repeat_keys("<enter>", "<backspace>")
+		end,
+	},
+}
