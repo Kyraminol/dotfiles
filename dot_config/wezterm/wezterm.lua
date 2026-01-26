503418751e5ce2d9b7b469fa05d0cb38257b2cb9
Kyraminol Endyeran <kyraminari@gmail.com>
2026-01-26 19:00:00 +0100

    [wezterm] update config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local config = {}
 
 config.font = wezterm.font("Cascadia Code NF", { weight="Regular", stretch="Normal", style="Normal" })
 config.enable_tab_bar = false
-config.default_prog = { 'pwsh', '-NoLogo' }
+config.default_prog = { 'nu' }
 config.window_padding = {
   left = '0.5cell',
   right = '0',
